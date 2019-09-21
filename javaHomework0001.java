3bd49587e80fc3e1af0c2a269647355ec59c9b7e
newhans <847135006@qq.com>
2019-09-21 20:46:07 +0800

    init project

---
+++ javaHomework0001.java
@@ -0,0 +1,18 @@
+class javaHomework0001{
+    public static void main(String args[]){
+        System.out.println("  |    1   2   3   4   5   6   7   8   9  10  11  12");
+        System.out.println("--+-------------------------------------------------");
+        System.out.println(" 1|    1   2   3   4   5   6   7   8   9  10  11  12");
+        System.out.println(" 2|        4   6   8  10  12  14  16  18  20  22  24");
+        System.out.println(" 3|            9  12  15  18  21  24  27  30  33  36");
+        System.out.println(" 4|               16  20  24  28  32  36  40  44  48");
+        System.out.println(" 5|                   25  30  35  40  45  50  55  60");
+        System.out.println(" 6|                       36  42  48  54  60  66  72");
+        System.out.println(" 7|                           49  56  63  70  77  84");
+        System.out.println(" 8|                               64  72  80  88  96");
+        System.out.println(" 9|                                   81  90  99 108");
+        System.out.println("10|                                      100 110 120");
+        System.out.println("11|                                          121 132");
+        System.out.println("12|                                              144");
+    }
+}
